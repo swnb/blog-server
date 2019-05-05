3cc1273b8048f4573af32e4ce96ced49e9960cbb
swnb <swnblovego@gmail.com>
2019-05-05 12:21:43 +0800

    add env config file

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,11 +2,13 @@ pub mod schema;
 pub mod table;
 
 use diesel::prelude::*;
+use std::env;
 
 pub fn connect() -> MysqlConnection {
-	let connection_url = "mysql://root:123@127.0.0.1:3243/tmp";
-
-	MysqlConnection::establish(&connection_url).unwrap_or_else(|_| {
-		panic!("can't connect with database");
-	})
+	match env::var("MYSQL_URL") {
+		Ok(connection_url) => MysqlConnection::establish(&connection_url).unwrap_or_else(|_| {
+			panic!("can't connect with database");
+		}),
+		Err(_) => panic!("can't get mysql connect url from env"),
+	}
 }
