c843709b5ee228e46296fd70e1d15311db9d501b
swnb <swnblovego@gmail.com>
2019-05-05 20:21:38 +0800

    less input

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ use actix_web::{server, App};
 fn main() {
 	server::new(|| {
 		let app = App::new();
-		let app = paper::handler(app);
-		app
+		paper::handler(app)
 	})
 	.bind("127.0.0.1:9999")
 	.expect("bind port 9999")
