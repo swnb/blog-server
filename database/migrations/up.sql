40ff814e0262866b0e1c6efd25ed93ab19bd8a2e
swnb <swnblovego@gmail.com>
2019-06-01 17:43:45 +0800

    add docker file with docker compose

--- database/migrations/up.sql
+++ database/migrations/up.sql
@@ -1,10 +1,11 @@
-create table  if not exists `papers` (
+create database `blog`;
+create table if not exists `papers` (
 	`id` int(11) auto_increment primary key,
 	`title` varchar(255) not null unique,
 	`content` text not null,
 	`author` varchar(255) not null,
 	`last_change_time` timestamp default now(),
-	`create_time` varchar(255),
+	`create_time` varchar(255) not null,
 	`tags` varchar(255) not null,
 	`hash` varchar(64) not null unique,
 	key `index_hash`(`hash`)
