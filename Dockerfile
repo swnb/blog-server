ceceb013a2e88806e67a403115da8ac8748ebd22
swnb <swnblovego@gmail.com>
2019-06-04 14:38:37 +0800

    fix bind addr

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM rust
 
 WORKDIR /app/blog/
 COPY . .
+ENV SERVER_PORT 80
 EXPOSE 80
 RUN cargo build --release
 
-CMD ["cargo","run","--release"]
+ENTRYPOINT [ "./target/release/blog" ]
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,26 @@ mod models;
 mod paper;
 mod utils;
 
-
 use actix_web::{server, App};
+use std::env;
 
 fn main() {
 	// set variable
 	utils::env::set_env();
 
+	let port = match env::var("SERVER_PORT") {
+		Ok(port) => port,
+		Err(_) => panic!("not SERVER_PORT env var set"),
+	};
+
+	let addr = String::from("0.0.0.0:") + &port;
+	println!("server is running at {}", addr);
+
 	server::new(|| {
 		let app = App::new();
 		paper::handler(app)
 	})
-	.bind("127.0.0.1:80")
-	.expect("bind port 80")
+	.bind(addr)
+	.expect("bind port fail")
 	.run();
 }
