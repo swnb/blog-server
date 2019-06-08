44e99a02d81c45cdce6fa73f0cf38c6b104ad864
swnb <swnblovego@gmail.com>
2019-06-08 19:03:26 +0800

    fix table error

--- database/mysql-init-files/table.sql
+++ database/mysql-init-files/table.sql
@@ -1,3 +1,4 @@
+use blog;
 create table if not exists `papers` (
 	`id` int(11) auto_increment primary key,
 	`title` varchar(255) not null unique,
@@ -6,12 +7,12 @@ create table if not exists `papers` (
 	`last_change_time` timestamp default now(),
 	`create_time` varchar(255) not null,
 	`hash` varchar(64) not null unique,
-	key `index_hash_content`(`hash`,`content`)
+	key `index_hash_content`(`hash`,`content`(255))
 );
 
 create table if not exists `paper_tags` (
 	`id` varchar(64) not null,
 	`tag` varchar(255) not null,
 	primary key(`id`,`tag`),
-	union key `index_tag_id` (`tag`,`id`)
+	unique key `index_tag_id` (`tag`,`id`)
 );
