14b13c977ad4f3e5371428151470ded28581c5c5
swnb <swnblovego@gmail.com>
2019-06-09 18:21:02 +0800

    add logger

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM rust
 WORKDIR /app/blog/
 COPY . .
 ENV SERVER_PORT 80
+ENV RUST_LOG actix_web=info
 EXPOSE 80
 RUN cargo build --release
 
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,23 @@ mod models;
 mod paper;
 mod utils;
 
-use actix_web::{server, App};
-use std::env;
+use actix_web::{middleware::Logger, server, App};
 
+use env_logger;
+use std::env;
 fn main() {
 	// set variable
 	utils::env::set_env();
+	env_logger::init();
 
 	let port = env::var("SERVER_PORT").expect("no SERVER_PORT env var set");
-
 	let addr = "0.0.0.0:".to_owned() + &port;
 	println!("server is running at {}", addr);
 
 	server::new(|| {
-		let app = App::new();
+		let app = App::new()
+			.middleware(Logger::default())
+			.middleware(Logger::new("%a %{User-Agent}i"));
 		paper::handler(app)
 	})
 	.bind(addr)
