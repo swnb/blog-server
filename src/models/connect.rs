71f160f6dbcbb1b851838c1de0edad8a03f120f9
swnb <swnblovego@gmail.com>
2019-07-27 22:16:19 +0800

    style : format with clippy

--- src/models/connect.rs
+++ src/models/connect.rs
@@ -1,25 +1,26 @@
 use diesel::pg::PgConnection;
+use lazy_static;
 use std::env;
 use std::{thread, time};
-use lazy_static;
 
 use r2d2;
 use r2d2_diesel::ConnectionManager;
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
-lazy_static!{
-	static ref connection_pool:Pool = create_connection();
+lazy_static! {
+	static ref connection_pool: Pool = create_connection();
 }
 
-pub fn get_connection()->Pool{
- 	connection_pool.clone()
+pub fn get_connection() -> Pool {
+	connection_pool.clone()
 }
 
 // get connection;
 fn create_connection() -> Pool {
 	let mut connect_counter = 0;
 	loop {
-		let conncet_url = env::var("DATABASE_URL").expect("can't get database connect url from env");
+		let conncet_url =
+			env::var("DATABASE_URL").expect("can't get database connect url from env");
 
 		let manager = ConnectionManager::<PgConnection>::new(conncet_url);
 		match r2d2::Pool::builder().build(manager) {
