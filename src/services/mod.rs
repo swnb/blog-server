d499fdee946f39dfa0b7c82bf528a56f9a399b39
swnb <swnblovego@gmail.com>
2019-07-30 17:57:37 +0800

    feat : add app state

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,9 +1,13 @@
 mod paper;
 use actix_web::{web, Scope};
+use std::{collections::HashSet, sync::RwLock};
 
 // regist all routes with actix_web scope
 pub fn init() -> Scope {
-	let scope = web::scope("/api/v1/blog");
+	let app_state = paper::AppState {
+		token_set: RwLock::new(HashSet::new()),
+	};
+	let scope = web::scope("/api/v1/blog").data(app_state);
 	paper::routes()
 		.into_iter()
 		.fold(scope, |scope, (path, route)| scope.route(path, route))
